7785c6dc4a4658e559e660441f58c3e206bffbd6
Ryan <ryanp.0422@gmail.com>
2016-09-16 16:57:45 -0500

    completed first draft

--- FishingLocationGPS.Client/MainPage.xaml.cs
+++ FishingLocationGPS.Client/MainPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -22,9 +23,36 @@ namespace FishingLocationGPS
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        private UserControls.AddLocation ucAddLocation;
+
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        private void HamburgerButton_Click(object sender, RoutedEventArgs e)
+        {
+            MySplitView.IsPaneOpen = !MySplitView.IsPaneOpen;
+        }
+
+        async private void StackPanel_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            var clickedPanel = (StackPanel)sender;
+            var name = clickedPanel.Name.ToUpper();
+            var dialog = new MessageDialog(name);
+            await dialog.ShowAsync();
+            var controls = control_display.Children;
+            foreach (var control in controls)
+            {
+                control_display.Children.Remove(control);
+            }
+            switch (name)
+            {
+                case "ADD":
+                    if (ucAddLocation == null) ucAddLocation = new UserControls.AddLocation();
+                    control_display.Children.Add(ucAddLocation);
+                    break;
+            }
         }
     }
 }
---
+++ FishingLocationGPS.Models/DbModels/Location.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Devices.Geolocation;
+
+namespace FishingLocationGPS.Models.DbModels
+{
+    public class Location
+    {
+        [Key]
+        public Guid LocationGuid { get; set; }
+
+        [Required]
+        public Geopoint GPSLocation { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public string Notes { get; set; }
+    }
+}
